5180e61bd6a91939ca92a035ddb46d50e93cce89
ytokmakoff <ytokmakoff@gmail.com>
2024-06-25 18:41:18 +0300

    first commit

---
+++ src/main/java/org/example/tasktracker/TaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.tasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
